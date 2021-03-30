4261e1ec52d813380d620019f6d18bc96fa10ca5
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-03-30 15:56:56 +0100

    Update main.py
    add resources folder, fixes #4

--- build/main.py
+++ build/main.py
@@ -10,6 +10,7 @@ except Exception as e:
     print("Directory exists, skipping")
 
 shutil.copytree(basePath + "/scripts", basePath + "/buildOut/overrides/scripts")
+shutil.copytree(basePath + "/resources", basePath + "/buildOut/overrides/resources")
 shutil.copytree(basePath + "/config", basePath + "/buildOut/overrides/config")
 shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/manifest.json")
 shutil.make_archive("build/client", "zip", basePath + "/buildOut")
