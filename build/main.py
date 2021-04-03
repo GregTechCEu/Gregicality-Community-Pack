8fce5de97d7ad0ab8c62d6441502b1575e3c5329
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-04-03 09:37:05 +0100

    less jank fix

--- build/main.py
+++ build/main.py
@@ -4,15 +4,19 @@ import shutil
 # if there is a problem with building, please ping htmlcsjs#0209 on discord
 
 basePath = os.path.realpath(__file__)[:-7] + ".."
+copyDirs = ["/scripts", "/resources", "/config", "/mods"]
 
 try:
     os.makedirs(basePath + "/buildOut/overrides")
 except Exception as e:
     print("Directory exists, skipping")
 
-shutil.copytree(basePath + "/scripts", basePath + "/buildOut/overrides/scripts")
-shutil.copytree(basePath + "/resources", basePath + "/buildOut/overrides/resources")
-shutil.copytree(basePath + "/config", basePath + "/buildOut/overrides/config")
+for dir in copyDirs:
+    try:
+        shutil.copytree(basePath + dir, basePath + "/buildOut/overrides" + dir)
+    except Exception as e:
+        print("Directory exists, skipping")
+
 shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/manifest.json")
 shutil.make_archive("build/client", "zip", basePath + "/buildOut")
 
