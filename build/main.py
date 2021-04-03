85105fb9f333f8dc37753c14e52a5fb653594e83
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-04-03 20:34:22 +0100

    Update main.py

--- build/main.py
+++ build/main.py
@@ -1,15 +1,23 @@
 import os
 import shutil
+import requests
 
 # if there is a problem with building, please ping htmlcsjs#0209 on discord
 
+ae2url = "https://github.com/PrototypeTrousers/Applied-Energistics-2/releases/download/omni-fixes-v34b/appliedenergistics2-rv6-stable-7.omni-fixes-v34b.jar"
 basePath = os.path.realpath(__file__)[:-7] + ".."
 copyDirs = ["/scripts", "/resources", "/config", "/mods"]
 
 try:
     os.makedirs(basePath + "/buildOut/overrides")
+    os.makedirs(basePath + "/mods")
 except Exception as e:
     print("Directory exists, skipping")
+
+r = requests.GET(ae2url)
+
+with open(baseDir + "/mods/appliedenergistics2-rv6-stable-7.omni-fixes.jar", "w") as jar:
+    jar.write(r.content)
 
 for dir in copyDirs:
     try:
