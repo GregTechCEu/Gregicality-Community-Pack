ad95407ca1638bf6b041b3309e113633b03b5423
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-04-03 08:34:19 +0100

    quick and dirty fix for build

--- build/main.py
+++ build/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+# if there is a problem with building, please ping htmlcsjs#0209 on discord
 
 basePath = os.path.realpath(__file__)[:-7] + ".."
 
@@ -12,7 +13,6 @@ except Exception as e:
 shutil.copytree(basePath + "/scripts", basePath + "/buildOut/overrides/scripts")
 shutil.copytree(basePath + "/resources", basePath + "/buildOut/overrides/resources")
 shutil.copytree(basePath + "/config", basePath + "/buildOut/overrides/config")
-shutil.copytree(basePath + "/mods", basePath + "/buildOut/overrides/mods")
 shutil.copy(basePath + "/manifest.json", basePath + "/buildOut/manifest.json")
 shutil.make_archive("build/client", "zip", basePath + "/buildOut")
 
