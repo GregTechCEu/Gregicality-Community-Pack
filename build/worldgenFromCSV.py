15386c0bbb07bc468ad546b3d8a1d6653df181b8
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-09-01 18:19:43 +0100

    fix worldgen issues

--- build/worldgenFromCSV.py
+++ build/worldgenFromCSV.py
@@ -105,6 +105,7 @@ for row in data:
     vein["min_height"] = int(row[7].split("-")[0])
     vein["generator"] = {"radius": [0]}
     vein["generator"]["radius"][0] = int(row[8].split("-")[0])
+    vein["generator"]["type"] = "ellipsoid"
     vein["density"] = float(row[9])
     vein["vein_populator"] = {}
     vein["vein_populator"]["material"] = "_".join(row[5].lower().split())
